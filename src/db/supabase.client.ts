e2596579119c711d1fc77aa6eeb879c5c8a58f40
Maciek-M <maciek.morawski@gmail.com>
2025-05-02 12:33:17 +0200

    userId  based on databse created user

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -7,4 +7,4 @@ const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
 export type SupabaseClient = typeof supabaseClient;
 
-export const DEFAULT_USER_ID = "43454c13-032d-4a61-8f7c-356fab613472";
+export const DEFAULT_USER_ID = "41b3a9d2-10b0-4b85-b523-ac7abb6042c9";
